daec801a398f5375aa44a872a20ba0e3cd908ac7
unknown <ethemk10k@gmail.com>
2022-07-21 13:34:25 +0300

    dictionary

---
+++ 4.Gun-Odev/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Gun_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> names = new MyDictionary<int, string>();
+            names.Add(1, "Tahiri");
+            names.Add(2, "Engin");
+
+            names.ShowList();
+        }
+
+        public class MyDictionary<T, K>
+        {
+            KeyValuePair<T, K>[] items; // KeyValuePair int gibi string gibi bir veri tipidir. 
+
+            public MyDictionary()
+            {
+                items = new KeyValuePair<T, K>[0];
+            }
+            public void Add(T _key, K _value)
+            {
+                if (Control(_key))
+                {
+                    Configuration(_key);
+                    items[items.Length - 1] = new KeyValuePair<T, K>(_key, _value); // En son bize gelen değeri dizimize aktarıp işi bitirdik.
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz key değeri özel olmalı. Yani daha önceden eklenmiş bir key değerini tekrardan kullanamassınız.");
+                }
+
+            }
+
+            public void ShowList()
+            {
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.Key + " " + item.Value);
+                }
+
+            }
+
+            private void Configuration(T _key)
+            {
+                KeyValuePair<T, K>[] tempArray = items;  // Geçici dizimizi oluşturduk
+                items = new KeyValuePair<T, K>[items.Length + 1];
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    items[i] = tempArray[i];  //Geçici dizideki elemanları alıp asıl dizimize gri koyduk.
+                }
+
+            }
+            private bool Control(T _Key)
+            {
+                bool control = true;
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i].Key.ToString() == _Key.ToString())
+                    {
+                        control = false;
+                    }
+                }
+                return control;
+            }
+        }
+    }
+}
---
+++ 4.Gun-Odev/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("4.Gun-Odev")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4.Gun-Odev")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("e390ea57-f5c7-4ba9-8a17-29d423711472")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
